7656861a7ab3f1d32cacbf100471693917b7f82c
mejunga <mejunga.azir@gmail.com>
2025-04-19 22:59:49 +0300

    Complete core page without CSS files

--- src/main/java/com/example/gamecatalog/GameCatalogApp.java
+++ src/main/java/com/example/gamecatalog/GameCatalogApp.java
@@ -4,26 +4,30 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 public class GameCatalogApp extends Application {
     @Override
-    public void start(Stage primaryStage) throws Exception 
-    {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/game-catalog-view.fxml"));
+    public void start(Stage primaryStage) throws Exception{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/game-catalog-view.fxml"));
+        Parent root = loader.load();
+        GameCatalogController controller = loader.getController();
+        controller.setStage(primaryStage);
+
     
         Scene scene = new Scene(root);
+        scene.setFill(Color.TRANSPARENT);
         primaryStage.initStyle(StageStyle.UNDECORATED);
-        primaryStage.setMinHeight(650);
-        primaryStage.setMinWidth(1100);
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+        primaryStage.setMinHeight(660);
+        primaryStage.setMinWidth(1110);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
-    public static void main(String[] args) 
-    {
+    public static void main(String[] args){
         launch(args);
     }
 }
